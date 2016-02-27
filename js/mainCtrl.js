a68964bbfd627dc2a59fdae731dd1e5acf63146d
Kyle Prestgard <ktprestgard@gmail.com>
2016-02-27 11:53:00 -0700

    more adjustments

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,14 @@
 app.controller('mainCtrl', function($scope){
 
-	$scope.name = 'Kyle';
+	$scope.name = [
+		'Kyle',
+		'Jenn',
+		'Austin',
+		'Dane'
+	]
+
+	$scope.p1 = 'Kyle',
+	$scope.p2 = 'Jenn',
+	$scope.p3 = 'Austin';
 
 });
--- js/timeDirective.js
+++ js/timeDirective.js
@@ -1,7 +1,10 @@
 app.directive('showTime', function(){
   return {
     restrict: 'E',
-    template: '<div> The current time is {{time}} </div>',
+    template: '<div> Hello {{name}}, the current time is {{time}} </div>',
+    scope: {
+    	name: '=pName'
+    },
     link: function(scope, element, attrs){
       var currentTime = new Date();
       scope.time = currentTime.toString();
